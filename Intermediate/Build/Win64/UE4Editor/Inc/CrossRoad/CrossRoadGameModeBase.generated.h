0fdf2a2f6abd67adbabe833687cea57320d66c4c
Nikita Khvan <111285264+NikitaKhvan@users.noreply.github.com>
2025-08-23 15:06:06 +0500

    Add project files.

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/CrossRoad/CrossRoadGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CROSSROAD_CrossRoadGameModeBase_generated_h
+#error "CrossRoadGameModeBase.generated.h already included, missing '#pragma once' in CrossRoadGameModeBase.h"
+#endif
+#define CROSSROAD_CrossRoadGameModeBase_generated_h
+
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_SPARSE_DATA
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_RPC_WRAPPERS
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACrossRoadGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrossRoadGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrossRoadGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CrossRoad"), NO_API) \
+	DECLARE_SERIALIZER(ACrossRoadGameModeBase)
+
+
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACrossRoadGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrossRoadGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrossRoadGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CrossRoad"), NO_API) \
+	DECLARE_SERIALIZER(ACrossRoadGameModeBase)
+
+
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrossRoadGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrossRoadGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrossRoadGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrossRoadGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrossRoadGameModeBase(ACrossRoadGameModeBase&&); \
+	NO_API ACrossRoadGameModeBase(const ACrossRoadGameModeBase&); \
+public:
+
+
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrossRoadGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrossRoadGameModeBase(ACrossRoadGameModeBase&&); \
+	NO_API ACrossRoadGameModeBase(const ACrossRoadGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrossRoadGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrossRoadGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrossRoadGameModeBase)
+
+
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_12_PROLOG
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_SPARSE_DATA \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_RPC_WRAPPERS \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_INCLASS \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_SPARSE_DATA \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CROSSROAD_API UClass* StaticClass<class ACrossRoadGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CrossRoad_Source_CrossRoad_CrossRoadGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/CrossRoad/CrossRoadGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CrossRoadGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CROSSROAD_API ACrossRoadGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
